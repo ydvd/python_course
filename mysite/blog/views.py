88cc8fa5c067e2f1f0ae484fd5ac1b3eccbb0c59
Yannick van Dolen <ydvd1308@gmail.com>
2023-01-06 17:59:06 +0100

    This should be the changes before the oopsie

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+def post(request):
+    return HttpResponse("I'm a post shourt and stout")
