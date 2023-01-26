3c063c607cc846dfc4d25b50ea91826b3a7ddd35
Yannick van Dolen <ydvd1308@gmail.com>
2023-01-26 17:08:34 +0100

    pandas data with pyplot

--- datavis/datacreation.py
+++ datavis/datacreation.py
@@ -9,4 +9,18 @@ raw_data = {'names':  ['nick', 'panda', 's', 'ari', 'valos'],
 df = pandas.DataFrame(raw_data, columns=['names', 'jan_ir', 'feb_ir', 'mar_ir'])
 df['total_ir'] = df['jan_ir'] + df['feb_ir'] + df['mar_ir']
 
+colors = [  (1, .4, .4), 
+            (1, .6, 1),
+            (.5, .3, 1),
+            (.3, 1, .5),
+            (.7, .7, .2)]
+
+plotter.pie(df['total_ir'],
+            labels=df['names'],
+            colors=colors,
+            autopct='%1.1f%%')
+
+plotter.axis('equal')
+
 print(df)
+plotter.show()
