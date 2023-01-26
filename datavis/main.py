1139cd640707065a5b114ac937e502476e885b2a
Yannick van Dolen <ydvd1308@gmail.com>
2023-01-26 16:10:42 +0100

    Configure charts

--- datavis/main.py
+++ datavis/main.py
@@ -5,11 +5,16 @@ pops =  [ 2.525, 2.758, 3.018, 3.322, 3.682, 4.061, 4.440, 4.853, 5.310, 5.735, 
 
 deaths = [1.2, 1.7, 1.8, 2.2, 2.5, 2.7, 2.9, 3, 3.1, 3.3, 3.5, 3.8, 4, 4.3] # Completely made up
 
-pyplot.plot(years, pops, color=(1, 0.3, 0.3))
-pyplot.plot(years, deaths, '--', color=(0.6, 0.6, 1))
+# pyplot.plot(years, pops, color=(1, 0.3, 0.3))
+# pyplot.plot(years, deaths, '--', color=(0.6, 0.6, 1))
+
+lines = pyplot.plot(years, pops, years, deaths)
 
 pyplot.ylabel("Population in billions")
 pyplot.xlabel("Years")
 pyplot.title("Population growth")
+pyplot.grid(True)
+
+pyplot.setp(lines, color=(1, .4, .4), marker="o")
 
 pyplot.show()
