27e36397bc25e631550cedd7c3cd49b7c6338528
Yannick van Dolen <ydvd1308@gmail.com>
2023-02-09 22:07:31 +0100

    funy mesag

--- speechrecog/feedbacksounds.py
+++ speechrecog/feedbacksounds.py
@@ -1,5 +1,7 @@
 import pyglet
 
+# Tutorial goes over using pyaudio as he couldn't get pyglet to work 
+# but turns out pyglet is pretty easy so there we go
 file = pyglet.resource.media('audio/start.mp3')
 
 file.play()
